f81d6fb6649c97967da95f27c64b2ea88eb28113
Muragu <muragumary1@gmail.com>
2024-09-24 10:28:25 +0300

    initial commit

---
+++ includes/models.h
@@ -0,0 +1,17 @@
+#ifndef models_h
+#define models_h
+
+#include <string>
+#include <vector>
+#include <map>
+#include <any>
+#include <memory>
+//#include <funtion>//
+
+class Field;
+class Setquery;
+class Database;
+class Model{
+
+}
+#endif
